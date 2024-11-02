ab0ccdd5dd9c85a0945693275d145083d7d25753
necrolingus <leigh.williams88@gmail.com>
2024-11-02 11:37:15 +0200

    date changes

--- routes/uiRoutes.js
+++ routes/uiRoutes.js
@@ -12,7 +12,9 @@ function computeExpiry(ttl) {
         day: 'numeric',
         hour: '2-digit',
         minute: '2-digit',
-        second: '2-digit'
+        second: '2-digit',
+        timeZone: 'GMT',
+        timeZoneName: 'short'
     };
     const formattedExpiryDate = expiryDate.toLocaleString('en-GB', options);
     return formattedExpiryDate
